f88fb1f87fc113301bd8057caebe180a1a4d6925
Fernando dos Santos Alvarenga <670600568@senacinformatica.br>
2023-11-23 20:34:22 -0300

    Finalização, teste ok

--- scrip.js
+++ scrip.js
@@ -1,1 +1,21 @@
-@import url("https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=" + valor de dentro do input)
+const container = document.querySelector('.container'),
+  Input = document.querySelector('.form input'),
+  btn = container.querySelector('.form button'),
+  Img = container.querySelector('.qr-code img');
+
+btn.addEventListener('click', () => {
+  let qrValue = Input.value;
+  if (!qrValue) return;
+  btn.innerText = "Gerador QR Code"
+  Img.src = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${qrValue}`;
+  Img.addEventListener('load', () => {
+    container.classList.add('active');
+    btn.innerText = "Gerar QR Code"
+  })
+});
+
+Input.addEventListener('keyup', () =>{
+  if(!Input.value){
+    container.classList.remove('active');
+  }
+})
